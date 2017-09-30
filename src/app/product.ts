1529fcf3b05c212b5cf3d8d2691a9e12050e09b8
Diana Tokarska <tokarskadiana@gmail.com>
2017-09-30 11:07:35 +0200

    Fix product.ts

---
+++ src/app/product.ts
@@ -0,0 +1,6 @@
+export class Product{
+    title:string;
+    description:string='';
+    price:number;
+    image:string='';
+}
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+  constructor() { }
+
+  @Input() product:Product;
+  ngOnInit() {
+  }
+
+}
